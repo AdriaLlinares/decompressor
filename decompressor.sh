a5f98574ad7f968a2bec8ac5fd9e8ac12d77353f
Adria Llinares Arroyo <adria.llinares@enti.cat>
2025-06-19 23:18:37 +0200

    first commit

---
+++ decompressor.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+ctrl_c(){
+  echo -e "\n\n [!] Saliendo...\n"
+  exit 1
+}
+# Ctrl+C
+trap ctrl_c INT
+
+descomprimir_gz(){
+file_name=""
+
+echo -e " [+] Que fixero .gz quieres descomprimir: \n"
+
+archivos=$(ls *.gz 2>/dev/null)
+
+if [ -z "$archivos" ]; then 
+  echo -e "\n [!] No se ha encontrado ningun archivo .gz en el directorio actual.\n"
+  return
+else
+  echo "$archivos"
+fi
+
+
+echo -e "\n [+] Introduce el nombre del archivo que deseas descomprimir: \n"
+
+read file_name 
+
+decompressed_file_name="$(7z l "$file_name" | tail -n 3 | head -n1 | awk '$NF{print $NF}')"
+
+7z x "$file_name" &>/dev/null
+last_valid_name="$decompressed_file_name"
+
+while [ $decompressed_file_name ]; do 
+  echo -e "\n [+] Nuevo archivo descomprimido: $decompressed_file_name"
+  7z x "$decompressed_file_name" &>/dev/null
+  last_valid_name="$decompressed_file_name"
+  decompressed_file_name="$(7z l "$decompressed_file_name" 2>/dev/null | tail -n 3 | head -n1 | awk '$NF{print $NF}')"
+done
+
+echo -e "\n\n[^_^] Archivo final: $last_valid_name"
+}
+
+descomprimir_tar(){
+
+  file_name=""
+
+echo -e " [+] Que fixero .tar quieres descomprimir: \n"
+
+archivos=$(ls *.tar 2>/dev/null)
+
+if [ -z "$archivos" ]; then
+  echo -e "\n [!] No se ha encontrado ningun archivo .tar en el directorio actual.\n"
+  return
+else
+  echo "$archivos"
+fi
+
+echo -e "\n [+] Introduce el nombre del archivo que deseas descomprimir: \n"
+
+read file_name 
+
+decompressed_file_name="$(7z l "$file_name" | tail -n 3 | head -n1 | awk '$NF{print $NF}')"
+
+7z x "$file_name" &>/dev/null
+last_valid_name="$decompressed_file_name"
+
+while [ $decompressed_file_name ]; do 
+  echo -e "\n [+] Nuevo archivo descomprimido: $decompressed_file_name"
+  7z x "$decompressed_file_name" &>/dev/null
+  last_valid_name="$decompressed_file_name"
+  decompressed_file_name="$(7z l "$decompressed_file_name" 2>/dev/null | tail -n 3 | head -n1 | awk '$NF{print $NF}')"
+done
+
+echo -e "\n\n[^_^] Archivo final: $last_valid_name"
+}
+menu(){
+  opcion=""
+
+  echo -e "\n [+] En que formato esta el fixero. \n"
+
+  echo -e "\n   [1] .gz\n"
+  echo -e "\n   [2] .tar\n"
+  read opcion
+
+  if [ "$opcion" == "1" ]; then 
+    descomprimir_gz
+  elif [ "$opcion" == "2" ]; then
+    descomprimir_tar  
+  fi
+}
+
+menu  
